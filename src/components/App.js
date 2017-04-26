16169c51bed10bd9c47d90fb3d7a3abf50e23a77
Samir Medjdoub <contact@codeconcept.fr>
2017-04-26 11:28:58 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Liste de commissions</h3>
+                <div>en contruction ...</div>
+            </div>
+        );
+    }
+}
+
+export default App;

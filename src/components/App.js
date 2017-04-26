d0f81c2b1c7cce45b95ba7ef762f631a610b1888
Samir Medjdoub <contact@codeconcept.fr>
2017-04-26 16:57:39 +0200

    adding a redux store

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,57 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import Form from './Form';
 import Itemlist from './ItemList';
 
 class App extends React.Component {
-    state = { 
-        articles: []
-    }
+    // state = { 
+    //     articles: []
+    // }
 
-    addArticle = (article) => {
-        let oldArticles = this.state.articles;
-        article.id = this.state.articles.length + 1;
-        let newArticles = [...oldArticles, article];
-        this.setState({ articles: newArticles});        
-    }
+    // addArticle = (article) => {
+    //     let oldArticles = this.state.articles;
+    //     article.id = this.state.articles.length + 1;
+    //     let newArticles = [...oldArticles, article];
+    //     this.setState({ articles: newArticles});        
+    // }
 
     render() {
         return (
             <div>
-                <h1>Liste de commissions</h1>
+                <h1>Liste de courses</h1>
                 <div>
-                    < Form formTitle="Ajouter des articles à acheter" addArticle={this.addArticle} />
+                    < Form formTitle="Ajouter des articles à acheter" addArticle={this.props.addArticle} />
                     <br />
                     <hr />
-                    < Itemlist articles={this.state.articles} />
+                    < Itemlist articles={this.props.articles} />
                 </div>
             </div>
         );
     }
 }
 
-export default App;
+
+const mapStateToProps = (state) => {
+  return {
+      articles: state.articles
+  };
+};
+
+
+const addArticleActionCreator = (article) => {
+    return {
+        type: 'ADD_ARTICLE',
+        payload: article
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addArticle: (article) => {
+            dispatch(addArticleActionCreator(article))
+        }
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const articlesReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'ADD_ARTICLE':
+            console.log('ADD_ARTICLE reducer');
+            console.log('action ', action);
+            action.payload.id = Date.now();
+            const newState = [...state, action.payload];
+            return newState;
+        default:
+            return state;    
+    }
+};
+
+const store = createStore(combineReducers({articles: articlesReducer}));
+console.log('store', store);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
